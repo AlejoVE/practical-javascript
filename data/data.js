19bf46c01e657e6547066a801b560e655f3eea8d
Luis Alejandro Montilla <luis_alejandro_499@hotmailcom>
2020-03-29 19:39:18 +0200

    I created the data folder with the file data.js
    I added an object with properties and methods

---
+++ data/data.js
@@ -0,0 +1,38 @@
+var  todoList = { 
+  todos: [],
+  addTodo: function (todoText) {
+      this.todos.push({
+          todoText: todoText,
+          completed: false
+      });
+  },
+  changeTodo: function (position, todoText) {
+      this.todos[position].todoText = todoText;
+  },
+  deleteTodo: function (position) {
+      this.todos.splice(position, 1);
+  }, 
+  toggleCompleted: function(position){
+      var todo = this.todos[position];
+      todo.completed = !todo.completed;
+  },
+  toggleAll: function(){
+     var totalTodos = this.todos.length;
+     var completedTodos = 0;
+
+     this.todos.forEach(function(todo){
+      if(todo.completed === true){
+          completedTodos++;
+      }
+     })
+
+     this.todos.forEach(function(todo){
+         if(completedTodos === totalTodos){
+             todo.completed = false;
+         }else{
+          todo.completed = true;
+         }
+      });
+         
+  }
+};
